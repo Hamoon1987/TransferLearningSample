8dd4135b12825764af39a82fd72a0b57badf92c7
Hamoon <hamoon.jafarian@ontariotechu.net>
2022-06-24 14:29:35 -0400

    Add test

--- test.py
+++ test.py
@@ -10,13 +10,18 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = fetch_model().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr = 0.0001)
 
+
+try:
+    os.makedirs('./test')
+except:
+    assert(os.path.isdir('./test'))
+img_name = os.listdir("./test")
 checkpoint = torch.load(latest_file)
 model.load_state_dict(checkpoint['model_state_dict'])
 optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 epoch = checkpoint['epoch']
 model.eval()
-
-img = Image.open("test/02.jpg")
+img = Image.open("./test/" + img_name[-1])
 transform = transforms.Compose([transforms.Resize((224,224)),
                         transforms.ToTensor(),
                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
